2cf57b1c0209bbca8842ede0ff4d8753ff0a5183
Kabi <Kabilan_Shanmuga@epam.com>
2022-05-29 22:54:58 +0530

    Add Solution

--- cache.go
+++ cache.go
@@ -2,22 +2,61 @@ package cache
 
 import "time"
 
+type Item struct {
+	v string
+	isInf bool
+	deadline time.Time
+}
+
 type Cache struct {
+	m map[string]Item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		m : make(map[string]Item),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	now := time.Now()
+	data, ok := c.m[key]
+	if !ok {
+		return "", ok
+	}
+	if !data.isInf {
+		notExp := now.Before(data.deadline)
+		if notExp {
+			return data.v, true
+		} else {
+			delete(c.m, key)
+			return "", false
+		}
+	}
+	return data.v, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	 c.m[key] = Item{
+		 v : value,
+		 isInf: true,
+	 }
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() (ks []string) {
+	now := time.Now()
+	ks = make([]string, 0, len(c.m))
+	for k, v := range c.m {
+		if now.Before(v.deadline) {
+			ks = append(ks, k)
+		}
+	}
+	return
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.m[key] = Item {
+		v : value,
+		deadline: deadline,
+	}
 }
