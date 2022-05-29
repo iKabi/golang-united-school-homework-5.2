90001c950d5a9636c5de34da38b1924a3f7fa2b9
Kabi <Kabilan_Shanmuga@epam.com>
2022-05-29 23:02:10 +0530

    Fix errors

--- cache.go
+++ cache.go
@@ -12,38 +12,38 @@ type Cache struct {
 	m map[string]Item
 }
 
-func NewCache() Cache {
-	return Cache{
+func NewCache() *Cache {
+	return &Cache{
 		m : make(map[string]Item),
 	}
 }
 
-func (c Cache) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
 	now := time.Now()
-	data, ok := c.m[key]
+	val, ok := c.m[key]
 	if !ok {
 		return "", ok
 	}
-	if !data.isInf {
-		notExp := now.Before(data.deadline)
-		if notExp {
-			return data.v, true
+	if !val.isInf {
+		ok := now.Before(val.deadline)
+		if ok {
+			return val.v, true
 		} else {
 			delete(c.m, key)
 			return "", false
 		}
 	}
-	return data.v, ok
+	return val.v, ok
 }
 
-func (c Cache) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
 	 c.m[key] = Item{
 		 v : value,
 		 isInf: true,
 	 }
 }
 
-func (c Cache) Keys() (ks []string) {
+func (c *Cache) Keys() (ks []string) {
 	now := time.Now()
 	ks = make([]string, 0, len(c.m))
 	for k, v := range c.m {
@@ -54,7 +54,7 @@ func (c Cache) Keys() (ks []string) {
 	return
 }
 
-func (c Cache) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
 	c.m[key] = Item {
 		v : value,
 		deadline: deadline,
